32639fe1a770504f4532f131869d85fc60a617b3
Jesse Brito <jesse_brito@me.com>
2018-05-22 15:00:41 -0700

    Made changes to JS for working "Start" button

--- js/main.js
+++ js/main.js
@@ -1,57 +1,83 @@
 let $board = $('.board');
 let $currentShip = null;
+let $boats;
 
 // For loop to create the 64 box playing grid
 for(i = 0; i <= 63; i++) {
     let box = document.createElement('div')
     box.classList.add('box')
-    $board.append(box)
+    $board.hide().append(box)
 }
+
+// Shows grid when clicking start
+let $startBtn = $('.start');
+$startBtn.on('click', function() {
+    $board.show(500);
+    setTimeout(function() {
+        alert('Player 1 set your boats!');
+    }, 600);
+    $(this).off();
+})
 
 let shipsLeft = 6;
 let $boxes = $('.box')
 let $turn = $('#turn')
+
 // Places ship in box clicked.
 let clickBoxHandler = function() {
-    $boxes.one('click', function() {
+    $boxes.on('click', function() {
         $(this).prepend($('<img>',{class:'boat', src:'images/littleboat.png'}))
         shipsLeft--;
-        console.log(shipsLeft)
-        var $boats = $('.boat')
+        $boats = $('.boat')
         if(shipsLeft === 3) {
             $boats.hide(1000)
-            turnHandler();
         } else {
             if(shipsLeft === 0) {
-                alert('FIGHT!')
-                console.log($boats.length)
                 $boats.hide(600)
-                startGame();
+                $boxes.off('click')
+                $turn.text('Turn: Player 1')
             }
         }
+        $(this).off('click')
     })
-    
 }
 clickBoxHandler();
-// Controls player turns
-function turnHandler() {
-    alert(`Player 2's turn`)
-    $turn.text('Turn: Player 2')
-    console.log($turn)
-}
 
-// let $boats = $("<img>id='boat', src='images/littleboat.png'</img>");
-// function startGame() {
-//     $board.css('opacity', '$boats', '0');
-// }
-
+// Clears board to begin a new game when clicking "New Game Button"
 $startNew = $('#new-game');
-
 $startNew.on('click', function() {
     $boxes.html("");
+    clickBoxHandler();
     shipsLeft = 6;
     $turn.text('Turn: Player 1')
 })
+
+// Shows boat when boat is clicked
+function boatHit() {
+    $boxes.on( "click", function( event ) {
+        event.preventDefault();
+        $(this).find('img').show(500);
+        console.log("Image Hit!")
+    });
+}
+
+//* function startGame() {
+//     let $startButton = $('#start')
+//     $startButton.on('click', function() {
+//         alert('FIGHT!')
+//     })
+// }
+
+//* Controls player turns
+// function turnHandler() {
+//     $(this).on('click', function() {
+//         if($turn.text() === 'Turn: Player 1') {
+//             $turn.text('Turn: Player 2')
+//         } else {
+//             $turn.text('Turn: Player 1')
+//         }
+//     })
+// };
 
 // Maybe place 2 images in html file & when 
 // shipsLeft reaches 0 switch to the next image(I could also change
